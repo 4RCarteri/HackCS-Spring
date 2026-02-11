5536b3fa8d1e9088910aacd2dbf079c92eb76dd5
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2026-02-11 15:26:50 -0300

    Initial commit

---
+++ src/main/java/com/example/HackCS_Spring/HackCsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.HackCS_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackCsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackCsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HackCS_Spring/HackCsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HackCS_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackCsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
